214574f9456180a43a86afd247af717b8a7115e0
annahileta <anna.hileta@sigma.software>
2021-12-01 17:01:54 +0200

    fix for issue with looping

--- DocuSign.MyClick/DocuSign.MyClickwrap.COVID19Waiver/ClientApp/src/index.js
+++ DocuSign.MyClick/DocuSign.MyClickwrap.COVID19Waiver/ClientApp/src/index.js
@@ -2,10 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 import "./i18n";
 import "bootstrap/dist/js/bootstrap.min.js";
-
+import { unregister } from "./registerServiceWorker";
 const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href");
 const rootElement = document.getElementById("root");
 
@@ -16,4 +15,4 @@ ReactDOM.render(
   rootElement
 );
 
-registerServiceWorker();
+unregister();
